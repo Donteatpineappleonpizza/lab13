427eafb622f473e6950d0ac9e2e430999ee650d6
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2021-12-24 18:35:02 +0700

    131

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 using namespace std;
+
+long long int fibonacci( long long int ) ;
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci ( long long int x ){
+if(x > 1){
+    x = fibonacci(x-1) + fibonacci(x-2) ;
+}
+return x ;
 }
